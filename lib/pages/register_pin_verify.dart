b578f0b0e8ed5b4a84d2a585d72ef2af16bd8095
Kenny Baltazar Alanoca <innovaappstar@gmail.com>
2020-07-25 11:28:24 -0500

    snackbar e inputs con cardview added

--- lib/pages/register_pin_verify.dart
+++ lib/pages/register_pin_verify.dart
@@ -17,25 +17,43 @@ class RegisterPinVerify extends StatelessWidget {
     borderSide: BorderSide(color: Colors.transparent),
   );
 
+  /*
+   * retorna el tamanio total de la pantalla sin considerar el tamanio del
+   * la barra de notificaciones o notch..
+   * pero si considera el safe area : (padding.top, padding.bottom)
+   */
+  double _screenTotalSizeWithoutAppBar(MediaQueryData mediaQueryData) {
+    return (mediaQueryData.size.height -
+        mediaQueryData.padding.top -
+        mediaQueryData.padding.bottom);
+  }
+
   @override
   Widget build(BuildContext context) {
-    FocusManager.instance.primaryFocus.unfocus();
-    return Scaffold(
-      body: SafeArea(
+    var mediaQuery = MediaQuery.of(context);
+    var heightTop = _screenTotalSizeWithoutAppBar(mediaQuery) * 0.5;
+    return Scaffold(body: new Builder(
+        // Create an inner BuildContext so that the onPressed methods
+        // can refer to the Scaffold with Scaffold.of().
+        builder: (BuildContext context) {
+      return SafeArea(
         child: Container(
           // color: Theme.of(context).primaryColor,
           child: Column(
-              // mainAxisSize: MainAxisSize.min,
-              // crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                _toolbarTop(context),
-                _descriptionTop(context),
-                // _phoneNumberInputBody(context),
-                // _phoneNumberInputBody(context),
-              ]),
+            children: <Widget>[
+              Container(
+                height: heightTop,
+                child: Column(children: [
+                  _toolbarTop(context),
+                  _descriptionTop(context),
+                ]),
+              ),
+              _middleBottomBody(context),
+            ],
+          ),
         ),
-      ),
-    );
+      );
+    }));
   }
 
   Widget _toolbarTop(BuildContext context) {
@@ -53,12 +71,38 @@ class RegisterPinVerify extends StatelessWidget {
       width: double.infinity,
       child: Column(
         children: <Widget>[
+          Text(
+            'Enter code',
+            style: Theme.of(context)
+                .textTheme
+                .headline5
+                .copyWith(color: Colors.black, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(
+            height: 10,
+          ),
           Container(
-            height: 60,
-            child: Image(
-              image: AssetImage(
-                AssetsUtils.ICON_VERIFICATION_PHONE_NUMBER,
-              ),
+            width: 200,
+            child: Row(
+              children: <Widget>[
+                Icon(
+                  Icons.ac_unit,
+                  color: Colors.green,
+                ),
+                Icon(
+                  Icons.ac_unit,
+                  color: Colors.green,
+                ),
+                Icon(
+                  Icons.ac_unit,
+                  color: Colors.green,
+                ),
+                Icon(
+                  Icons.ac_unit,
+                  color: Colors.green,
+                ),
+              ],
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             ),
           ),
           SizedBox(
@@ -78,15 +122,6 @@ class RegisterPinVerify extends StatelessWidget {
             ),
           ),
           _phoneNumberInputBody(context),
-          // Row(
-          //   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          //   children: <Widget>[
-          //     PINNumber(pinOneController, outlineInputBorder, (text) {}),
-          //     PINNumber(pinTwoController, outlineInputBorder, (text) {}),
-          //     PINNumber(pinThirdController, outlineInputBorder, (text) {}),
-          //     PINNumber(pinFourController, outlineInputBorder, (text) {}),
-          //   ],
-          // ),
         ],
       ),
     );
@@ -128,7 +163,7 @@ class RegisterPinVerify extends StatelessWidget {
                   ),
                 ),
                 SizedBox(
-                  height: 10,
+                  height: 15,
                 ),
                 Container(
                   width: double.infinity,
@@ -137,7 +172,12 @@ class RegisterPinVerify extends StatelessWidget {
                   ),
                   child: FlatButton(
                     child: new Text('Submit'),
-                    onPressed: () {},
+                    onPressed: () {
+                      // final snackBar =
+                      //     SnackBar(content: Text('Yay! A SnackBar!'));
+                      // Scaffold.of(context).showSnackBar(snackBar);
+                      createSnackBar(context, 'Title - subtitle');
+                    },
                     textColor: Colors.white,
                     color: Theme.of(context).primaryColor,
                     shape: new RoundedRectangleBorder(
@@ -155,37 +195,93 @@ class RegisterPinVerify extends StatelessWidget {
     );
   }
 
-  Widget _phoneNumberInputCard(
-      BuildContext context, String hintText, int maxLength, Function function) {
-    var mediaQuery = MediaQuery.of(context);
-    var widthInput = mediaQuery.size.width * 0.8;
-    return Container(
-      width: 60,
-      padding: EdgeInsets.symmetric(
-        horizontal: 10,
-      ),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.all(
-          Radius.circular(
-            5.0,
+  void createSnackBar(BuildContext context, String message) {
+    final snackBar = SnackBar(
+        content: Container(
+      height: 30,
+      child: Column(
+        children: <Widget>[
+          SizedBox(
+            height: 20.0,
+            child: FittedBox(
+                fit: BoxFit.fitHeight,
+                child: Text(
+                  message,
+                  style: Theme.of(context).textTheme.headline5,
+                )),
           ),
-        ),
-        border: Border.all(
-          color: Colors.grey[300],
-        ),
+          SizedBox(
+            height: 10.0,
+            child: FittedBox(
+                fit: BoxFit.fitHeight,
+                child: Text(
+                  message,
+                  style: Theme.of(context).textTheme.headline6,
+                )),
+          ),
+        ],
       ),
-      child: TextField(
-        textAlign: TextAlign.left,
-        decoration: new InputDecoration(
-          border: InputBorder.none,
-          hintText: hintText,
-          counterText: "",
-        ),
-        textInputAction: TextInputAction.done,
-        maxLength: maxLength,
-        keyboardType: TextInputType.numberWithOptions(decimal: true),
-        onChanged: function,
-        style: Theme.of(context).textTheme.headline6,
+    ));
+
+    // Find the Scaffold in the Widget tree and use it to show a SnackBar!
+    Scaffold.of(context).showSnackBar(snackBar);
+  }
+
+  Widget _middleBottomBody(BuildContext context) {
+    var mediaQuery = MediaQuery.of(context);
+    var heightBackground = _screenTotalSizeWithoutAppBar(mediaQuery) * 0.5;
+
+    return Container(
+      height: heightBackground,
+      child: Column(
+        children: <Widget>[
+          Expanded(
+            child: Container(),
+          ),
+          Text(
+            'Did not receive the code?',
+            style: Theme.of(context)
+                .textTheme
+                .headline6
+                .copyWith(color: Colors.grey, fontSize: 11),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              FlatButton(
+                onPressed: () {
+                  print("Hello world");
+                },
+                child: Text(
+                  'Resend',
+                  style: Theme.of(context)
+                      .textTheme
+                      .headline6
+                      .copyWith(color: Colors.blue),
+                ),
+              ),
+              FlatButton(
+                onPressed: () {
+                  print("Hello world");
+                },
+                // focusColor: Colors.green,
+                // highlightColor: Colors.purple,
+                // splashColor: Colors.purple,
+                child: Text(
+                  'Get a call now',
+                  style: Theme.of(context)
+                      .textTheme
+                      .headline6
+                      .copyWith(color: Colors.blue),
+                ),
+              )
+            ],
+          )
+        ],
       ),
     );
   }
